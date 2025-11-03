dae592832825365be208e8e9c888ee8f53cf4f4c
Nguyễn Hoàng Phong <nguyenhoangphong170108@gmail.com>
2025-11-03 22:57:26 +0700

    havent tested the translate logic, though have conducted mini tests so should be good

--- test.py
+++
@@ -1,6 +0,0 @@
-import easyocr
-import numpy as np
-reader = easyocr.Reader(['en'])
-result = reader.readtext('12.png', detail = 1)
-formatted = [(bbox, text) for bbox, text, _ in result]
-print(formatted)
--- translate.py
+++ translate.py
@@ -1,10 +1,47 @@
-import easyocr
+import requests
+import json
 import screenshot as sc
+from dotenv import load_dotenv
+import os
 
-class OCR:
-    def __init__(self, langlist = ['ja', 'en']):
-        self.reader = easyocr.Reader(langlist)
+load_dotenv()
+OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
+
+def unpack_text_and_bbox(result: list):
+    placeholder = [(bbox, text) for bbox, text, _ in result]
+    bbox_list = list(map(lambda x: x[0], placeholder))
+    text_to_translate = list(map(lambda x: x[1], placeholder))
+    return bbox_list, text_to_translate
+
+def process_text_for_api(text_to_translate : list):
+    text_for_translation = '\n'.join(text_to_translate)
+    return text_for_translation
+
+def translate_text(result: list):
+    bbox, preproccessed_text = unpack_text_and_bbox(result)
+    text = process_text_for_api(preproccessed_text)
+
+    url="https://openrouter.ai/api/v1/chat/completions"
+    headers={
+    "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+    "Content-Type": "application/json"
+  }
+
+    data={
+    "model": "cognitivecomputations/dolphin-mistral-24b-venice-edition:free", 
+    "messages": [
+      {
+        "role": "user",
+        "content": f"Translate this Japanese text to English. Provide only the translation, no explanations:\n\n{text}"
+      }
+    ]
+  }
     
-    def extract_text(self, img):
-        result = self.reader.readtext(img)
-        return result
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        result = response.json()
+        translated_text = result['choices'][0]['message']['content']
+        translated_text = translated_text.split()
+    else:
+        print('failed to get response from api')
+    return [(box, string) for box, string in zip(bbox, translated_text)]
